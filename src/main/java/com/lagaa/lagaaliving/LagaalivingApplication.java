00eeec22d6d01561c31823029e954e9ae562e0a2
swaleh <ethereumdevelopment@gmail.com>
2024-10-08 23:18:25 +0100

    spring Initialized

---
+++ src/main/java/com/lagaa/lagaaliving/LagaalivingApplication.java
@@ -0,0 +1,13 @@
+package com.lagaa.lagaaliving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagaalivingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LagaalivingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lagaa/lagaaliving/LagaalivingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagaa.lagaaliving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagaalivingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
